5bab368511d10f0eca4cca2ecba9dd1fde8a5609
shmemcat <emilycatellier@gmail.com>
2022-12-01 17:10:34 -0700

    Refactoring to simplify input sorting

--- 01/index.js
+++ 01/index.js
@@ -6,30 +6,21 @@ import { readFileSync } from 'node:fs';
 const input = readFileSync("./input.txt", {encoding: "UTF-8"} );
 
 // Split input on newline
-const inputSplit = input.split("\n")
-
-// Convert strings to integers
-let inputArray = [];
+const inputSplit = input.split("\n\n")
+let inputGroups = []
 for(let i = 0; i < inputSplit.length; i++) {
-   inputArray.push(parseInt(inputSplit[i]));
+   inputGroups.push(inputSplit[i].split("\n"))
 }
 
-// Group numbers into an array of arrays using NaN as delimeter
-let tempArray = [];
-let groupedInputArray = [];
-inputArray.map(x => {
-   if(x) {
-      tempArray.push(x)
+// Convert strings to integers
+let summedArray = [];
+for(let i = 0; i < inputGroups.length; i++) {
+   let temp = 0;
+   for(let j = 0; j < inputGroups[i].length; j++) {
+      temp += parseInt(inputGroups[i][j]);
    }
-   else {
-      groupedInputArray.push(tempArray)
-      tempArray = []
-   }
-})
-
-// Sum together the total value of each array within the array
-let summedArray = []
-groupedInputArray.map(y => { summedArray.push(y.reduce((acc, curr) => acc + curr, 0)); })
+   summedArray.push(temp);
+}
 
 /* ------------------ FUNCTIONS ------------------ */
 
