be0f072fa1c63fe321b4142c8a4ae49235e2f1d1
shmemcat <emilycatellier@gmail.com>
2022-12-02 22:45:50 -0700

    Can't even assume that much

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ import { readFileSync } from 'node:fs';
 /* ------------------ READ INPUT ------------------ */
 const input = readFileSync("./input.txt", {encoding: "UTF-8"} );
 
-// Split and group input
-const inputGroups = input.split("\n").map(n => n.split(" "));
-
 /* ------------------ FUNCTIONS ------------------ */
 
 // Solve Part One Function
