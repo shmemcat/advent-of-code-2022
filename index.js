ba0658aa62c40f52be742ab3afd7928b206e11ae
shmemcat <emilycatellier@gmail.com>
2022-12-02 22:34:43 -0700

    advent of code start file

---
+++ index.js
@@ -0,0 +1,24 @@
+/* ------------------ IMPORTS ------------------ */
+import { readFileSync } from 'node:fs';
+
+/* ------------------ READ INPUT ------------------ */
+const input = readFileSync("./input.txt", {encoding: "UTF-8"} );
+
+// Split and group input
+const inputGroups = input.split("\n").map(n => n.split(" "));
+
+/* ------------------ FUNCTIONS ------------------ */
+
+// Solve Part One Function
+function solvePartOne() {
+
+}
+
+// Solve Part Two Function
+function solvePartTwo() {
+
+}
+
+/* ------------------ FUNCTION CALLS ------------------ */
+//solvePartOne();
+//solvePartTwo();
