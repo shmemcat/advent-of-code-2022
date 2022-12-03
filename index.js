b942d31f6a9497a9877a3d06df959db35e79405b
shmemcat <emilycatellier@gmail.com>
2022-12-02 23:31:18 -0700

    More start file additions

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ const input = readFileSync("./input.txt", {encoding: "UTF-8"} );
 
 // Solve Part One Function
 function solvePartOne() {
-
+   //console.log(`Part One Solution: ${}`);
 }
 
 // Solve Part Two Function
 function solvePartTwo() {
-
+   //console.log(`Part Two Solution: ${}`);
 }
 
 /* ------------------ FUNCTION CALLS ------------------ */
