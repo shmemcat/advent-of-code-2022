533f436c8d378b288e63d4b501cc719779da2c6e
shmemcat <emilycatellier@gmail.com>
2022-12-05 22:59:07 -0700

    Less obvious comments

--- 06/index.js
+++ 06/index.js
@@ -9,13 +9,11 @@ const input = String(readFileSync("./input.txt", { encoding: "UTF-8" }));
 // Check for repeats in a 4 character string
 function checkFourCharSeqs(str) {
    let index = 3; // keep track of where the marker occurs after processing
-   // iterate over the characters of the string
-   for (let i = 0; i < str.length - 3; i++) {
-      // extract a four-character sequence from the string
-      let marker = str.substring(i, i + 4);
-      index++; // add one to the marker counter
 
-      // check if any of the characters in the sequence are repeated
+   for (let i = 0; i < str.length - 3; i++) {
+      let marker = str.substring(i, i + 4);
+      index++;
+
       if (!hasRepeats(marker)) { // if no repeats
          return index;
       }
@@ -26,11 +24,10 @@ function checkFourCharSeqs(str) {
 // Check for repeats in a 14 character string
 function checkFourteenCharSeqs(str) {
    let index = 13; // keep track of where the marker occurs after processing
-   // iterate over the characters of the string
+
    for (let i = 0; i < str.length - 13; i++) {
-      // extract a fourteen-character sequence from the string
       let marker = str.substring(i, i + 14);
-      index++; // add one to the marker counter
+      index++;
 
       // check if any of the characters in the sequence are repeated
       if (!hasRepeats(marker)) { // if no repeats
