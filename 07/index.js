a7c6ebd9c3591ebfa82a1beae383b24bae17a72b
shmemcat <emilycatellier@gmail.com>
2022-12-09 03:25:03 -0700

    Day 7 start files

---
+++ 07/index.js
@@ -0,0 +1,25 @@
+/* ------------------ IMPORTS ------------------ */
+import { readFileSync } from "node:fs";
+
+/* ------------------ READ INPUT ------------------ */
+const input = readFileSync("./input.txt", { encoding: "UTF-8" });
+
+/* ------------------ DATA FUNCTIONS ------------------ */
+
+/* ------------------ PARSING ------------------ */
+
+/* ------------------ SOLUTION FUNCTIONS ------------------ */
+
+// Solve Part One Function
+function solvePartOne() {
+   //console.log(`Part One Solution: ${}`);
+}
+
+// Solve Part Two Function
+function solvePartTwo() {
+   //console.log(`Part Two Solution: ${}`);
+}
+
+/* ------------------ FUNCTION CALLS ------------------ */
+//solvePartOne();
+//solvePartTwo();
